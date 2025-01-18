58527135738532e6cd8c5ce0763fd400f26425c0
Jiří Liška <70215056+liskajiri@users.noreply.github.com>
2025-01-18 11:55:15 +0100

    feat: Add hypothesis tests
    - removes 10k lines from a test file

--- python/needle/data/transforms.py
+++ python/needle/data/transforms.py
@@ -1,15 +1,15 @@
-import numpy as np
+import numpy as array_api
 
 from needle.backend_ndarray.ndarray import NDArray
 
 
 class Transform:
-    def __call__(self, x):
+    def __call__(self, x: NDArray) -> NDArray:
         raise NotImplementedError
 
 
 class RandomFlipHorizontal(Transform):
-    def __init__(self, p: float = 0.5):
+    def __init__(self, p: float = 0.5) -> None:
         self.p = p
 
     def __call__(self, img: NDArray) -> NDArray:
@@ -21,14 +21,14 @@ class RandomFlipHorizontal(Transform):
             H x W x C ndarray corresponding to image flipped with probability self.p
         Note: use the provided code to provide randomness, for easier testing
         """
-        # TODO: Transformations without numpy
-        if np.random.rand() < self.p:
-            return np.flip(img, axis=1)
+        # # TODO: Transformations without numpy
+        if array_api.random.random() < self.p:
+            return array_api.flip(img, axis=1)
         return img
 
 
 class RandomCrop(Transform):
-    def __init__(self, padding=3):
+    def __init__(self, padding: int = 3) -> None:
         self.padding = padding
 
     def __call__(self, img: NDArray) -> NDArray:
@@ -40,16 +40,18 @@ class RandomCrop(Transform):
         Note: generate the image shifted by shift_x, shift_y specified below
         """
         assert img.ndim == 3
-        shift_x, shift_y = np.random.randint(
+        shift_x, shift_y = array_api.random.randint(
             low=-self.padding, high=self.padding + 1, size=2
         )
+        # shift_x = random.randint(-self.padding, self.padding)
+        # shift_y = random.randint(-self.padding, self.padding)
 
         H, W, C = img.shape
 
         H += 2 * self.padding
         W += 2 * self.padding
 
-        img_padded = np.zeros((H, W, C))
+        img_padded = array_api.zeros((H, W, C))
         # copy img to the padded array
         img_padded[
             self.padding : H - self.padding, self.padding : W - self.padding, :
