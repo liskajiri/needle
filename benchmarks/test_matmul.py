63cab292ae85b3c494b44b2f8d5b5b7a347439d9
Jiří Liška <70215056+liskajiri@users.noreply.github.com>
2025-04-05 23:37:13 +0200

    test: readd benchmarks

--- benchmarks/test_matmul.py
+++ benchmarks/test_matmul.py
@@ -10,7 +10,6 @@ _ALL_DEVICES = [
     pytest.param(
         ndl.cuda(), marks=pytest.mark.skipif(not ndl.cuda().enabled(), reason="No GPU")
     ),
-    ndl.cpu_numpy(),
 ]
 
 # TODO: split to CPU/GPU separate tests
@@ -18,10 +17,8 @@ _ALL_DEVICES = [
 
 matmul_dims = [
     (8, 8, 8),
-    # (32, 32, 32),
-    # (64, 64, 64),
-    # (128, 128, 128),
-    # (256, 256, 256),
+    (64, 64, 64),
+    (128, 128, 128),
 ]
 
 
@@ -29,7 +26,7 @@ def matmul(A: NDArray, B: NDArray) -> NDArray:
     return A @ B
 
 
-@pytest.mark.parametrize("device", _ALL_DEVICES, ids=["cpu", "cuda", "np"])
+@pytest.mark.parametrize("device", _ALL_DEVICES, ids=["cpu", "cuda"])
 @pytest.mark.parametrize(("m", "n", "p"), matmul_dims)
 @pytest.mark.benchmark(
     max_time=1,
@@ -49,15 +46,34 @@ def test_matmul(benchmark, m, n, p, device) -> None:
     np.testing.assert_allclose(out.numpy(), a @ b, rtol=1e-4, atol=1e-4)
 
 
+@pytest.mark.parametrize(("m", "n", "p"), [(128, 128, 128)])
+@pytest.mark.benchmark(
+    max_time=1,
+    min_rounds=1000,
+    disable_gc=True,
+    warmup=True,
+    warmup_iterations=100,
+)
+def test_matmul_numpy(benchmark, m, n, p) -> None:
+    device = ndl.cpu_numpy()
+    a = rng.standard_normal((m, n))
+    b = rng.standard_normal((n, p))
+
+    A = ndl.array(a, device=device)
+    B = ndl.array(b, device=device)
+
+    out = benchmark(matmul, A, B)
+    np.testing.assert_allclose(out.numpy(), a @ b, rtol=1e-4, atol=1e-4)
+
+
 large_matmul_dims = [
     (256, 256, 256),
-    (512, 512, 512),
-    (1024, 1024, 1024),
+    # (512, 512, 512),
+    # (1024, 1024, 1024),
 ]
 
 
-@pytest.mark.skip(reason="Skip large matmul test for now")
-@pytest.mark.parametrize("device", _ALL_DEVICES, ids=["cpu", "cuda", "np"])
+@pytest.mark.parametrize("device", _ALL_DEVICES, ids=["cpu", "cuda"])
 @pytest.mark.parametrize(("m", "n", "p"), large_matmul_dims)
 @pytest.mark.benchmark(
     max_time=3,
